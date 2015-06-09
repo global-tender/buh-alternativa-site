0b05bb29f6c368dd1aa5f612576bc8e9bec87422
Igor Popov <ihptru@gmail.com>
2015-06-09 14:03:01 +0000

    contacts page changes

--- p/contacts/index.php
+++ p/contacts/index.php
@@ -25,43 +25,77 @@ include_once("../../header.php");
 				<p><i class="fa fa-envelope-o"></i> <a href="mailto:alternativa50@mail.ru">alternativa50@mail.ru</a> </p>
 				<p><i class="fa fa-clock-o"></i> Понедельник - пятница: 9:00 – 17:00</p>
 
-				<form name="sentMessage" id="contactForm novalidate">
+				<div id="map" style="width: 600px; height: 400px"></div>
+				<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+
+				<script type="text/javascript">
+					ymaps.ready(init);
+					var myMap, 
+						myPlacemark;
+
+					function init(){ 
+						myMap = new ymaps.Map("map", {
+							center: [47.219214, 39.708392],
+							zoom: 16
+						}); 
+						
+						myPlacemark = new ymaps.Placemark([47.219214, 39.708392], {
+							hintContent: 'Альтернатива',
+							balloonContent: 'Альтернатива: пр. Буденовский 26/57 оф. 14, г Ростов-на-Дону'
+						}, {
+							preset: 'islands#blueDotIcon'
+						});
+						
+						myMap.geoObjects.add(myPlacemark);
+					}
+				</script>
+
+				<form name="sentMessage" id="contactForm novalidate" method="POST" action="/p/contacts/">
 				<div class="col-md-12"><h3>Напишите нам:</h3></div>
 				<hr>
 				<div class="col-md-6">
 					<div class="control-group form-group">
 						<div class="controls">
-							<label>Ф.И.О:</label> <input class="form-control" id="name" required data-validation-required-message="Please enter your name.">
+							<label>Ф.И.О:</label> <input class="form-control" name="name" id="name" required data-validation-required-message="Пожалуйста, введите Ф.И.О.">
 							<p class="help-block"></p>
 						</div>
 					</div>
 					<div class="control-group form-group">
 						<div class="controls">
-							<label>Номер телефона:</label> <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
+							<label>Номер телефона:</label> <input type="tel" name="tel" class="form-control" id="phone" required data-validation-required-message="Пожалуйста, введите номер телефона.">
 						</div>
 					</div>
 					<div class="control-group form-group">
 						<div class="controls">
-							<label>Email:</label> <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
+							<label>Email:</label> <input type="email" name="email" class="form-control" id="email" required data-validation-required-message="Пожалуйста, введите E-Mail адрес.">
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="control-group form-group">
 						<div class="controls">
-							<label>Сообщение:</label> <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message" maxlength="999" style="resize:none"></textarea>
+							<label>Сообщение:</label> <textarea name="message" rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Поле 'Сообщение' не может быть пустым." maxlength="999" style="resize:none"></textarea>
 						</div>
 					</div>
 
 					<div id="success"></div> <!-- For success/fail messages -->
 
-					<button type="submit" class="btn btn-primary">Send Message</button>
+					<input type="submit" name="submit" class="btn btn-primary" value="Отправить сообщение">
 				</div>
 			</form>
 			</div>
 		</div>
 	</div>
 
+<?php
+
+if (isset($_POST['message']))
+{
+	$content = "Вам отправлено сообщение с сайта atv61.ru.\n\nИмя: {$_POST['name']}\nТелефон: {$_POST['tel']}\nE-Mail: {$_POST['email']}\n\nСообщение:\n\n{$_POST['message']}\n";
+	mail("ihptru@gmail.com", "Сообщение с сайта atv61.ru", $content);
+}
+
+?>
 <?php
 include_once("../../footer.php");
 ?>
